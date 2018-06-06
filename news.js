86151ad040a673796c30498b729598781d2022d4
mdroberts74 <mikedroberts88@gmail.com>
2018-06-06 11:27:32 -0500

    I added the push that will push my array into the object

--- news.js
+++ news.js
@@ -1,26 +1,30 @@
 const recentNews = [
-        H3News = {
-            h3: "Recent News Events",
-        },
+    H3News = {
+        h3: "Recent News Events",
+    },
 
-        newsImg = {
-            img: "https://ak3.picdn.net/shutterstock/videos/6137663/thumb/1.jpg"
+    newsImg = {
+        img: "https://ak3.picdn.net/shutterstock/videos/6137663/thumb/1.jpg"
 
-        },
+    },
 
-        eventsConnected = {
-            date: "May 21st 2018",
-            location: "Nashville TN.",
-            typeOfEvents: " A new sighting!",
-            victims: "Fred Bergins",
-            witnesses: "Billy Bardy",
-            agency: "TBI"
+    eventsConnected = {
+        date: "May 21st 2018",
+        location: "Nashville TN.",
+        typeOfEvents: " A new sighting!",
+        victims: "Fred Bergins",
+        witnesses: "Billy Bardy",
+        agency: "TBI"
 
-        }
+    }
 
 
-    ]
-    // console.log(recentNews)
+]
+
+intelReport.push(recentNews)
+
+
+// console.log(recentNews)
 
 // const saveDatabase = function(databaseObject, localStorageKey) {
 //     const stringifiedDatabase = JSON.stringify(databaseObject)
