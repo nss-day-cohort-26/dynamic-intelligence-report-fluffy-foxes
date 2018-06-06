beab64f3f4a3f1d1a731953e91b5492c93c9c270
mdroberts74 <mikedroberts88@gmail.com>
2018-06-06 11:15:56 -0500

    Created an array and two objects

---
+++ news.js
@@ -0,0 +1,28 @@
+const recentNews = [
+        H3News = {
+            h3: "Recent News Events",
+        },
+
+        newsImg = {
+            img: "https://ak3.picdn.net/shutterstock/videos/6137663/thumb/1.jpg"
+
+        },
+
+        eventsConnected = {
+            date: "May 21st 2018",
+            location: "Nashville TN.",
+            typeOfEvents: " A new sighting!",
+            victims: "Fred Bergins",
+            witnesses: "Billy Bardy",
+            agency: "TBI"
+
+        }
+
+
+    ]
+    // console.log(recentNews)
+
+// const saveDatabase = function(databaseObject, localStorageKey) {
+//     const stringifiedDatabase = JSON.stringify(databaseObject)
+//     localStorage.setItem(localStorageKey, stringifiedDatabase)
+// }
