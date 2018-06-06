bb2a206fd5a22a04107c9c62b908dd161eb0b026
Emily Venturino <ebventurino@gmail.com>
2018-06-06 13:18:27 -0500

    finished editing array

--- travel.js
+++ travel.js
@@ -1,32 +1,32 @@
+const travel = []
 
-const travel = [
-     title = {
+     const title = {
         h3: "Travel",
-    },
+    }
     
-    imgTravel = {
+    const imgTravel = {
         img: "https://pmcdeadline2.files.wordpress.com/2016/03/wheres-waldo.jpg?w=446&h=299&crop=1"
-    },       
-     airports = {
+    }     
+     const airports = {
         airport1: "Orlando International Airport",
         airport2: "San Francisco International Airport",
         airport3: "Louis Armstrong New Orleans International Airport",
-    },
-     busStations = {
+    }
+     const busStations = {
         busstation1: "Orlando Greyhound Bus Station",
         busstation2: "Transbay Transit Center",
         busstation3: "Gretna Bus Terminal",
-    },
-     citiesVisited = {
+    }
+     const citiesVisited = {
         city1: "Orlando",
         city2: "San Francisco",
         city3: "New Orleans"
-    },
-    rentalCompanies = {
+    }
+    const rentalCompanies = {
         rental1: "Enterprise Rent-A-Car",
         rental2: "The San Francisco Sailing Company",
         rental3: "Orlando Rentals Property Management Co. LLC"
 }
-]
+
 
 travel.push(title,imgTravel,airports,busStations,citiesVisited,rentalCompanies)
