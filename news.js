58c6b67bcdbe84f1c31190feaca576f9deea8545
mdroberts74 <mikedroberts88@gmail.com>
2018-06-06 13:13:39 -0500

    Made an empty array, added more objects

--- news.js
+++ news.js
@@ -1,65 +1,65 @@
-const recentNews = [
-    H3News = {
-        h3: "Recent News Events",
-    },
+const recentNews = []
+const H3News = {
+    h3: "Recent News Events",
+}
 
-    newsImg = {
-        img: "https://ak3.picdn.net/shutterstock/videos/6137663/thumb/1.jpg"
+const newsImg = {
+    img: "https://ak3.picdn.net/shutterstock/videos/6137663/thumb/1.jpg"
 
-    },
+}
 
-    event1 = {
-        date: "May 21st 2018",
-        location: "Nashville TN.",
-        typeOfEvents: " A new sighting!",
-        victims: "Fred Bergins",
-        witnesses: "Billy Bardy",
-        agency: "TBI",
-        rating: "100%"
+const event1 = {
+    date: "May 21st 2018",
+    location: "Nashville TN.",
+    typeOfEvents: " A new sighting!",
+    victims: "Fred Bergins",
+    witnesses: "Billy Bardy",
+    agency: "TBI",
+    rating: "100%"
 
-    },
-    event2 = {
-        date: "September 17, 2018",
-        location: "Nashville TN.",
-        typeOfEvents: " A new sighting!",
-        victims: "Fred Bergins",
-        witnesses: "Billy Bardy",
-        agency: "TBI",
-        rating: "100%"
+}
+const event2 = {
+    date: "September 17, 2018",
+    location: "Nashville TN.",
+    typeOfEvents: " A new sighting!",
+    victims: "Fred Bergins",
+    witnesses: "Billy Bardy",
+    agency: "TBI",
+    rating: "100%"
 
-    },
-    event3 = {
-        date: "August 25, 2017",
-        location: "Nashville TN.",
-        typeOfEvents: " A new sighting!",
-        victims: "Fred Bergins",
-        witnesses: "Billy Bardy",
-        agency: "TBI",
-        rating: "100%"
+}
+const event3 = {
+    date: "August 25, 2017",
+    location: "Nashville TN.",
+    typeOfEvents: " A new sighting!",
+    victims: "Fred Bergins",
+    witnesses: "Billy Bardy",
+    agency: "TBI",
+    rating: "100%"
 
-    },
-    event4 = {
-        date: "May 11, 2016",
-        location: "Nashville TN.",
-        typeOfEvents: " A new sighting!",
-        victims: "Fred Bergins",
-        witnesses: "Billy Bardy",
-        agency: "TBI",
-        rating: "100%"
+}
+const event4 = {
+    date: "May 11, 2016",
+    location: "Nashville TN.",
+    typeOfEvents: " A new sighting!",
+    victims: "Fred Bergins",
+    witnesses: "Billy Bardy",
+    agency: "TBI",
+    rating: "100%"
 
-    },
-    event5 = {
-        date: "June 11, 2014",
-        location: "Nashville TN.",
-        typeOfEvents: " A new sighting!",
-        victims: "Fred Bergins",
-        witnesses: "Billy Bardy",
-        agency: "TBI",
-        rating: "100%"
+}
+const event5 = {
+    date: "June 11, 2014",
+    location: "Nashville TN.",
+    typeOfEvents: " A new sighting!",
+    victims: "Fred Bergins",
+    witnesses: "Billy Bardy",
+    agency: "TBI",
+    rating: "100%"
 
-    }
-]
-console.log(recentNews)
+}
+
+recentNews.push(newsImg, event1, event2, event3, event4, event5)
 intelReport.push(recentNews)
 
 
