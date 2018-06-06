0ab8d9ce0828e66934821aabf22b18ac51fe4961
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-06-06 15:35:05 -0500

    merge conflict rsolved

--- rap.js
+++ rap.js
@@ -1,5 +1,4 @@
 const rapSheet = []
-<<<<<<< HEAD
 
 const h3Rap = {
     h3: "Rap Sheet"
@@ -16,23 +15,6 @@ const ulRap = {
 }
 
 rapSheet.push(h3Rap, imgRap, ulRap)
-=======
-
-    const h3Rap = {
-        h3: "Rap Sheet"
-    }
-    const imgRap = {
-        waldo: "http://static-0.gogamz.com/games/java/1/72/game-2/1-wheres-wally-now.webp"
-    }
-    const ulRap = {
-        crimes: "Loitering",
-        convictions: "Running from scene of the crime",
-        persons: "Grandma, Uncle Henry",
-        witnesses: "Thousands of people",
-        victims: "Carmen SanDiego"
-    }
-
->>>>>>> 0d32be6313361eabfebdee6647ddc4d39aa4b250
 
 rapSheet.push(h3Rap, imgRap, ulRap)
 intelReport.rapSheetKey = rapSheet
