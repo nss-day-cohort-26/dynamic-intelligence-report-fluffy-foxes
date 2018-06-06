69aa363e0874d7a7f206b84e888d9dbfd9e50517
Aaron Miller <millerac333@gmail.com>
2018-06-06 11:41:06 -0500

    created array

---
+++ finance.js
@@ -0,0 +1,23 @@
+const finance = [
+    const title = {
+        h3: "Fiancial Interests"
+    },
+    const banks = { 
+        bank1: "Imperial Bank",
+        bank2: "Pierce & Pierce Mergers and Acquisitions",
+        bank3: "Gringotts",
+    },
+    const business = {
+        business1: "Waldo's Cameo",
+        business2: "Waldo's Beanies",
+    },
+    const blackmail = {
+        person1: "Sundar Pichai",
+        person2: "Mark Zuckerberg", 
+        person3: "Odlaw", 
+        person4: "White Wizard"
+},
+    const shellBusiness = {
+        fakebusiness1: "Waldo's Information Security and Threat Protection"
+    }
+intelReport.push(finance)
