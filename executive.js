620a58f990ba6ba3955af0935370f8fc834f0ca0
Natasha Cox <natashac1014@gmail.com>
2018-06-06 11:14:48 -0500

    added nav and objects

---
+++ executive.js
@@ -0,0 +1,34 @@
+const exectutiveSummary = []
+
+const h3ES = {
+    titleES: "Executive Summary"
+}
+
+const pictureOfWaldo = {
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9VBCDTHRdECMdZ8A-tUQD-6_dFozIyh6HOowF1Fq_fAYDQAHB'
+
+}
+
+const nameofPerp = {
+    name: 'Waldo Walderson'
+}
+const countryOrigin = {
+    country: 'France'
+
+}
+
+const listOfAlias = {
+    alias1: 'Red Bandit',
+    alias2: 'Stripes',
+    alias3: 'Beanie Boy'
+
+}
+
+const knownAssociates = {
+    ass1: 'Sir, Eye-Spy',
+    ass2: 'Finders Keepers',
+    ass3: 'Lost and Found'
+
+}
+
+exectutiveSummary.push(h3ES, pictureOfWaldo, nameofPerp,countryOrigin, listOfAlias, knownAssociates)
