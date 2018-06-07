bc5a93812c64bad71434ce2a16ea338d29557054
Natasha Cox <natashac1014@gmail.com>
2018-06-07 09:16:17 -0500

    made h3 and img else if

--- functions.js
+++ functions.js
@@ -4,10 +4,20 @@ const createSection = (objectKey) => {
     console.log(articleKey)
     articleKey.forEach(item => {
         const articleSection = document.createElement('section')
-        for(let key in item) { //else if statement
+        for(let key in item) {
+            if (key === 'img') {
+                const imgElement = document.createElement('img')
+                imgElement.setAttribute('src', item[key])
+                articleSection.appendChild(imgElement)
+            } else if (key === h3) {
+                const h3Element = document.createElement('h3')
+                h3Element.textContent = `${item[key]}`
+                articleSection.appendChild(h3Element)
+            } else {
             const divElement = document.createElement('div')
             divElement.textContent = `${key}: ${item[key]}`
             articleSection.appendChild(divElement)
+            }
         }
         fragment.appendChild(articleSection)
     })
