f8edd4247ec83be14472164b9976352b8ec20277
mdroberts74 <mikedroberts88@gmail.com>
2018-06-07 11:40:46 -0500

    I shortned the news articles and added some details in the news details

--- rap.js
+++ rap.js
@@ -1,17 +1,17 @@
 const rapSheet = [
     const h3Rap = {
-        h3: "Rap Sheet"
-    },
-    const imgRap = {
-        waldo: "http://static-0.gogamz.com/games/java/1/72/game-2/1-wheres-wally-now.webp"
-    },
-    const ulRap = {
-        crimes: "Loitering",
-        convictions: "Running from scene of the crime",
-        persons: "Grandma, Uncle Henry",
-        witnesses: "Thousands of people",
-        victims: "Carmen SanDiego"
-    }
+            h3: "Rap Sheet"
+        },
+        const imgRap = {
+                waldo: "http://static-0.gogamz.com/games/java/1/72/game-2/1-wheres-wally-now.webp"
+            },
+            const ulRap = {
+                crimes: "Loitering",
+                convictions: "Running from scene of the crime",
+                persons: "Grandma, Uncle Henry",
+                witnesses: "Thousands of people",
+                victims: "Carmen SanDiego"
+            }
 
 ]
 
